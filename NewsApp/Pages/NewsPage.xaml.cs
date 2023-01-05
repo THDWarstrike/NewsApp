457ed8dfc756d4ee2f8e52b6765e0d2b7dab05a3
Keith McHugh <mchughkeith3@gmail.com>
2023-01-05 09:56:06 -0500

    edit

--- NewsApp/Pages/NewsPage.xaml.cs
+++ NewsApp/Pages/NewsPage.xaml.cs
@@ -1,9 +1,23 @@
+using NewsApp.Models;
+
 namespace NewsApp.Pages;
 
 public partial class NewsPage : ContentPage
 {
+	public List<Article> ArticleList { get; set; }
 	public NewsPage()
 	{
 		InitializeComponent();
+		ArticleList = new List<Article>();
+	}
+	protected override async void OnOppering()
+	{
+		base.OnAppearing();
+		ApiService apiService = new ApiService();
+		var newsResult = await apiService.GetNews();
+		foreach(var item in newsResult.Articles)
+		{
+			ArticlesList.Add(item);
+		}
 	}
 }
